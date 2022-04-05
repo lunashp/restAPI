fdc00554ccf4738e12ccee1754dc4cb7cff1a980
lunashp <hijnshp@gmail.com>
2022-04-05 15:54:38 +0900

    restAPI

--- src/main/java/kr/co/hjsoft/config/SecurityConfiguration.java
+++ src/main/java/kr/co/hjsoft/config/SecurityConfiguration.java
@@ -36,10 +36,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/*/users").hasRole("ADMIN")
                 .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
             .and()
-//                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
-//            .and()
-//                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-//            .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+            .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+            .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // jwt token 필터를 id/password 인증 필터 전에 넣어라.
 
     }
